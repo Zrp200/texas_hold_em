5f583375250097f22acf81e39de0469f9dca93ae
Zachary Perlmutter <zrp200@gmail.com>
2014-08-13 13:26:48 -0700

    Update texas_hold_em.rb

--- lib/texas_hold_em.rb
+++ lib/texas_hold_em.rb
@@ -1,3 +1,63 @@
-module Computer
-	# The chance something will happen
-		class Odds
+# Everything related to how to computer plays poker
+	module Computer
+		# The chance something will happen
+			class Odds
+				attr_accessor :deck, :hand
+				def initialize(deck, hand); @deck, @hand = deck, hand; end
+				# The best starting hand; 2 Aces
+					def best_hand
+						aces = 0
+						cards = deck.cards.length
+						for card in deck.cards
+							if card.num == "Ace"
+								aces += 1
+							end
+						end
+						return aces/cards
+					end
+			end
+			class Player < Player
+				def action(amt, pot=pot)
+					initial = amt + @bet
+					if amt == 0 
+						false
+					elsif amt + bet < pot
+						nil
+					elsif amt + bet > pot && amt + bet > @money
+						true
+					elsif amt
+				
+	end
+	class Player
+		attr_accessor :bet, :money	
+	end
+# The deck. It stores Card objects.
+	class Deck
+		# The actual cards
+			attr_reader :cards
+		def initialize
+			@cards = Array.new
+			4.times do
+				@cards.push Card.new("Ace")
+				@cards.push Card.new("Jack")
+				@cards.push Card.new("Queen")
+				@cards.push Card.new("King")
+				for num in (2..10).to_a
+					@cards.push Card.new(num)
+				end
+			end
+		end
+	end
+# The main object. These objects are used to make game decisions.
+	class Card
+		def initialize(num)
+			@num, @value = num, case num
+				when "Ace" then 14
+				when "King" then 13
+				when "Queen" then 12
+				when "Jack" then 11
+			else
+				num.to_i
+			end
+		end
+	end
